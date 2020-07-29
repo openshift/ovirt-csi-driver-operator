11a0982d804dd05d9b7931f323ebd2dd0fffe7c2
Benny Zlotnik <bzlotnik@redhat.com>
2020-07-29 22:41:51 +0300

    address PR comments
    - Rewrite changes as new controller
    - image pull policy IfNotPresent
    - remove unneeded rule
    - use errors instead of fatal
    
    Signed-off-by: Benny Zlotnik <bzlotnik@redhat.com>

--- pkg/operator/starter.go
+++ pkg/operator/starter.go
@@ -1,7 +1,6 @@
 package operator
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 
@@ -11,20 +10,12 @@ import (
 	"github.com/openshift/library-go/pkg/controller/controllercmd"
 	"github.com/openshift/library-go/pkg/operator/csi/csicontrollerset"
 	goc "github.com/openshift/library-go/pkg/operator/genericoperatorclient"
-	"github.com/openshift/library-go/pkg/operator/resource/resourceapply"
-	"github.com/openshift/library-go/pkg/operator/staticresourcecontroller"
 	"github.com/openshift/library-go/pkg/operator/v1helpers"
-	ovirtsdk "github.com/ovirt/go-ovirt"
-	corev1 "k8s.io/api/core/v1"
-	storagev1 "k8s.io/api/storage/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime/serializer/json"
+	"github.com/ovirt/csi-driver-operator/pkg/generated"
 	dynamicclient "k8s.io/client-go/dynamic"
 	kubeclient "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/klog"
-	"k8s.io/client-go/kubernetes/scheme"
-	"github.com/ovirt/csi-driver-operator/pkg/generated"
 )
 
 const (
@@ -36,9 +27,8 @@ const (
 )
 
 type CSIOperator struct {
-	ovirtClient  *ovirt.Client
-	nodeName     *string
-	storageClass *storagev1.StorageClass
+	ovirtClient *ovirt.Client
+	nodeName    *string
 }
 
 func NewCSIOperator(nodeName *string) (*CSIOperator, error) {
@@ -54,63 +44,6 @@ func NewCSIOperator(nodeName *string) (*CSIOperator, error) {
 	}, nil
 }
 
-func (o *CSIOperator) getStorageDomain(ctx context.Context, kubeClient *kubeclient.Clientset) string {
-	get, err := kubeClient.CoreV1().Nodes().Get(ctx, *o.nodeName, metav1.GetOptions{})
-	if err != nil {
-		klog.Fatal(err)
-	}
-	nodeID := get.Status.NodeInfo.SystemUUID
-
-	conn, err := o.ovirtClient.GetConnection()
-	if err != nil {
-		klog.Fatal(err)
-	}
-
-	vmService := conn.SystemService().VmsService().VmService(nodeID)
-	attachments, err := vmService.DiskAttachmentsService().List().Send()
-	if err != nil {
-		klog.Fatal(err)
-	}
-
-	for _, attachment := range attachments.MustAttachments().Slice() {
-		if attachment.MustBootable() {
-			d, _ := conn.FollowLink(attachment.MustDisk())
-			disk, ok := d.(*ovirtsdk.Disk)
-			klog.Info(fmt.Sprintf("Extracting Storage Domain from disk %s", disk.MustId()))
-
-			if !ok {
-				klog.Fatal("Could not fetch disk")
-			}
-
-			s, _ := conn.FollowLink(disk.MustStorageDomains().Slice()[0])
-			sd, ok := s.(*ovirtsdk.StorageDomain)
-
-			klog.Info(fmt.Sprintf("Fetched Storage Domain %s", sd.MustName()))
-			if !ok {
-				klog.Fatal("Could not fetch storage domain")
-			}
-
-			return sd.MustName()
-		}
-	}
-
-	return ""
-}
-
-func (o *CSIOperator) addStorageClass(ctx context.Context, kubeClient *kubeclient.Clientset) {
-	sdName := o.getStorageDomain(ctx, kubeClient)
-
-	// Create StorageClass after the fact since we need to figure out a default Storage Domain for it
-	storageClass := generateStorageClass(sdName)
-	existingStorageClass, _ := kubeClient.StorageV1().StorageClasses().Get(ctx, storageClass.Name, metav1.GetOptions{})
-	if existingStorageClass != nil {
-		klog.Info("Storage Class already exists, removing...")
-		kubeClient.StorageV1().StorageClasses().Delete(ctx, storageClass.Name, metav1.DeleteOptions{})
-	}
-
-	o.storageClass = storageClass
-}
-
 func (o *CSIOperator) RunOperator(ctx context.Context, controllerConfig *controllercmd.ControllerContext) error {
 	// Create clientsets and informers
 	kubeClient := kubeclient.NewForConfigOrDie(rest.AddUserAgent(controllerConfig.KubeConfig, operatorName))
@@ -123,7 +56,10 @@ func (o *CSIOperator) RunOperator(ctx context.Context, controllerConfig *control
 		return err
 	}
 
-	o.addStorageClass(ctx, kubeClient)
+	if err != nil {
+		klog.Errorf(fmt.Sprintf("Could not add storage class: %v"))
+		return err
+	}
 
 	csiControllerSet := csicontrollerset.NewCSIControllerSet(
 		operatorClient,
@@ -170,12 +106,12 @@ func (o *CSIOperator) RunOperator(ctx context.Context, controllerConfig *control
 		csicontrollerset.WithNodeService("node.yaml"),
 	)
 
-	scController := staticresourcecontroller.NewStaticResourceController(
-		"StorageClass",
-		o.encoder,
-		[]string{"storageclass.yaml"},
-		(&resourceapply.ClientHolder{}).WithKubernetes(kubeClient),
+	scController := NewOvirtStrogeClassController(
 		operatorClient,
+		kubeClient,
+		kubeInformersForNamespaces,
+		o.ovirtClient,
+		*o.nodeName,
 		controllerConfig.EventRecorder,
 	)
 
@@ -193,41 +129,4 @@ func (o *CSIOperator) RunOperator(ctx context.Context, controllerConfig *control
 	<-ctx.Done()
 
 	return fmt.Errorf("stopped")
-}
-
-func generateStorageClass(storageDomainName string) *storagev1.StorageClass {
-	reclaimPolicy := corev1.PersistentVolumeReclaimDelete
-	var expected = &storagev1.StorageClass{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "ovirt-csi-sc",
-			Namespace: defaultNamespace,
-		},
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "StorageClass",
-			APIVersion: "storage.k8s.io/v1",
-		},
-		// ObjectMeta will be filled below
-		Provisioner:          instanceName,
-		Parameters:           map[string]string{"storageDomainName": storageDomainName, "thinProvisioning": "true"},
-		ReclaimPolicy:        &reclaimPolicy,
-		MountOptions:         []string{},
-		AllowVolumeExpansion: boolPtr(false),
-	}
-	expected.Annotations = map[string]string{
-		"storageclass.kubernetes.io/is-default-class": "true",
-	}
-	return expected
-}
-
-func boolPtr(val bool) *bool {
-	return &val
-}
-
-func (o *CSIOperator) encoder(s string) ([]byte, error) {
-	ser := json.NewYAMLSerializer(json.DefaultMetaFactory, scheme.Scheme, scheme.Scheme)
-	var bt []byte
-	bf := bytes.NewBuffer(bt)
-	ser.Encode(o.storageClass, bf)
-
-	return bf.Bytes(), nil
 }
